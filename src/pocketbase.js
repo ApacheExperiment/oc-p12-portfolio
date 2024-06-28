fd767b0de404dd85702e9a76b4ec545c24ed3689
Apache <apache_macadam@hotmail.com>
2024-06-28 21:52:53 +0200

    mise à jour adresse server

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const url = new PocketBase('https://ab-portfolio.pockethost.io/');
+const url = ('https://ab-portfolio.pockethost.io/');
 const pb = new PocketBase(url)
 
 export default pb;
