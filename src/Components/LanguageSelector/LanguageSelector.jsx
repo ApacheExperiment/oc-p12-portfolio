70f142f4b7cf8b4f22f2bff7a8b13096c0a97156
Apache <apache_macadam@hotmail.com>
2024-06-29 19:16:42 +0200

    correction logo translate

--- src/Components/LanguageSelector/LanguageSelector.jsx
+++ src/Components/LanguageSelector/LanguageSelector.jsx
@@ -1,28 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './languageSelector.scss';
-import iconTranslateReverse from '../../assets/img/icon/Reverse/icon-translate-Reverse.webp';
-import iconTranslate from '../../assets/img/icon/Initial/icon-translate.webp';
+import iconTranslate from '../../assets/img/icon/icon-translate.webp';
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  // Hook pour détecter les changements du mode sombre
-  useEffect(() => {
-    const matchMedia = window.matchMedia('(prefers-color-scheme: dark)');
-
-    const handleChange = (e) => {
-      setIsDarkMode(e.matches);
-    };
-
-    setIsDarkMode(matchMedia.matches);
-    matchMedia.addEventListener('change', handleChange);
-
-    return () => {
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, []);
 
   const toggleLanguage = () => {
     const newLanguage = i18n.language === 'fr' ? 'en' : 'fr';
@@ -32,7 +14,7 @@ const LanguageSelector = () => {
   return (
     <div className="Translate__wrap">
       <button className="Translate__buttons" onClick={toggleLanguage}>
-        <img src={isDarkMode ? iconTranslate : iconTranslateReverse} 
+        <img src={iconTranslate}
         alt="icon de traduction"
         className="Translate__icon"/>
         {i18n.language === 'fr' ? 'FR' : 'EN'}
